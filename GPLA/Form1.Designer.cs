e2210d68541f5a8c3992fd05aa2ed68a9fb0ee82
Cryerjw01 <56078733+Cryerjw01@users.noreply.github.com>
2019-10-18 12:10:35 +0100

    Master

---
+++ GPLA/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace GPLA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.command = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Run = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.display = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.display)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // command
+            // 
+            this.command.Location = new System.Drawing.Point(12, 418);
+            this.command.Name = "command";
+            this.command.Size = new System.Drawing.Size(372, 20);
+            this.command.TabIndex = 0;
+            this.command.KeyDown += new System.Windows.Forms.KeyEventHandler(this.command_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(307, 207);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Load";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Run
+            // 
+            this.Run.Location = new System.Drawing.Point(390, 416);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(75, 23);
+            this.Run.TabIndex = 4;
+            this.Run.Text = "Run";
+            this.Run.UseVisualStyleBackColor = true;
+            this.Run.Click += new System.EventHandler(this.button3_Click);
+            this.Run.Paint += new System.Windows.Forms.PaintEventHandler(this.Run_Paint);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(29, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(261, 368);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // display
+            // 
+            this.display.Location = new System.Drawing.Point(435, 39);
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(353, 368);
+            this.display.TabIndex = 6;
+            this.display.TabStop = false;
+            this.display.Click += new System.EventHandler(this.display_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.display);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Run);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.command);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.display)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox command;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Run;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox display;
+    }
+}
+
---
+++ GPLA/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GPLA
+{
+    public partial class Form1 : Form
+    {
+       
+        Pen pen = new Pen(Color.Blue, 5);
+
+        public Form1()
+        {
+            InitializeComponent();
+            display.Image = new Bitmap(Size.Width, Size.Height);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String input = command.Text;
+            if (input.Trim() == "")
+            {
+                MessageBox.Show("Enter a command", "ERROR");
+            }
+            else
+            {
+                input = command.Text.Trim();
+                int x = 0;
+                using (var g = Graphics.FromImage(display.Image))
+                {
+                    Pen myPen = new Pen(Color.Pink, 2);
+                    switch (input.ToLower())
+                    {
+                        case "circle":
+                            MessageBox.Show("You Drew a Circle", "Messgae");
+
+
+                            g.DrawEllipse(myPen, 50, 50, 200, 100);
+                            
+
+                            break;
+                        case "rectangle":
+                            MessageBox.Show("You Drew a Rectangle", "Message");
+                            g.DrawRectangle(myPen,0, 0, 50, 20);
+                            break;
+                        case "squear":
+                            MessageBox.Show("You Drew a Rectangle", "Message");
+                            g.DrawRectangle(myPen, 0, 30, 50, 50);
+                            break;
+
+                        case "line":
+                            MessageBox.Show("Your Drew a Line", "Message");
+                            g.DrawLine(myPen, 0, 20, 20, 50);
+                            break;
+                        case "clear":
+                            g.Clear(Color.Transparent);
+                            break;
+                        default:
+                            MessageBox.Show("This is not a Shape", "MEessgae");
+                            x = -1;
+                            break;
+
+                    }
+                    display.Refresh();
+                    
+                } if (x == -1)
+                {
+
+                }
+                else {
+                Button btn = sender as Button;
+                listBox1.Items.Add(input);
+                command.Clear();
+                    x = 0;
+                }
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void command_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Run.PerformClick();
+                e.SuppressKeyPress = true;
+                e.Handled = true;
+            }
+        }
+        private void drawCircle(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics; // get a reference to Graphics object
+            Pen myPen = new Pen(Color.Pink, 2);
+
+
+            g.DrawEllipse(myPen, 50, 50, 200, 100);
+
+        }
+
+
+        private void Run_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void display_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void display_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
