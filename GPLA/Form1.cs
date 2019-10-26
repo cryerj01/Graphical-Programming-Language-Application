ddf3cbf851c4254fb703b1449bd394017af53cca
Cryerjw01 <56078733+Cryerjw01@users.noreply.github.com>
2019-10-26 18:44:30 +0100

    allaws command to have diffrent informarion aftet eg circle 20 has a radius of 20. notes added for to do next line 139

--- GPLA/Form1.cs
+++ GPLA/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,19 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Forms;
+
+
+
 
 namespace GPLA
 {
+    
     public partial class Form1 : Form
     {
-       
-        Pen pen = new Pen(Color.Blue, 5);
+
+        private Pen pen = new Pen(Color.Pink, 2);
 
         public Form1()
         {
@@ -29,10 +35,7 @@ namespace GPLA
 
         }
 
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
+        
 
         private void button3_Click(object sender, EventArgs e)
         {
@@ -44,107 +47,106 @@ namespace GPLA
             else
             {
                String[] lines = ControlePanel.Lines.ToArray();
+               // MessageBox.Show(lines.ToString(), "test");
                 
-               
+                ArrayList Currentline = new ArrayList();
+                String[] line;
+                int i = 0;
+
+                while (lines.Length != i) {
+
+                    line = lines[i].Split(' ');
+                    Currentline.Add(line);
+                    i++;
+                }
                 int count = 0;
                 using (var g = Graphics.FromImage(display.Image))
+                {
+                    while (lines.Length >= count)
+                    {
+                        
 
-                    
-                    while (lines.Length >= count) {
-                        Pen myPen = new Pen(Color.Pink, 2); 
-                       
-                        Regex regex = new Regex(@"\(([^\)]+)\)");
-
+                       Pen myPen = new Pen(Color.Pink, 2);
+                        
                         try
                         {
-                            String output = regex.Replace(lines[count], "");
-
-
-                            
-                            string line = lines[count];
-                           // string insideParentheses = Regex.Match(line, "([0-9][0-9][0-9])").Groups(0).ToString();
-                            switch (output.ToLower())
+                            string[] element = (String[])Currentline[count];
+                            int j = 0;
+                            switch (element[j].ToLower())
                             {
                                 case "circle":
                                     MessageBox.Show("You Drew a Circle", "Messgae");
-                                    g.DrawEllipse(myPen, 50, 50, 200, 100);
-                                    count++;
+                                    int radius = int.Parse(element[1]);
+                                    Circle(radius);                                    
                                     break;
                                 case "rectangle":
                                     MessageBox.Show("You Drew a Rectangle", "Message");
-                                    g.DrawRectangle(myPen, 0, 0, 50, 20);
-                                    count++;
+                                    int width = int.Parse(element[1]);
+                                    int hight = int.Parse(element[2]);
+                                    Rectangle(width, hight);
+                                                                       
                                     break;
                                 case "squear":
                                     MessageBox.Show("You Drew a Rectangle", "Message");
-                                    g.DrawRectangle(myPen, 0, 30, 50, 50);
-                                    count++;
+                                    g.DrawRectangle(myPen, 0, 30, 50, 50);                                    
                                     break;
                                 case "line":
                                     MessageBox.Show("Your Drew a Line", "Message");
-                                    g.DrawLine(myPen, 0, 20, 20, 50);
-                                    count++;
+                                    g.DrawLine(myPen, 0, 20, 20, 50);                                    
                                     break;
                                 case "clear":
-                                    g.Clear(Color.Transparent);
-                                    count++;
+                                    g.Clear(Color.Transparent);                                    
                                     break;
                                 default:
-                                    MessageBox.Show(output + lines[count] + " not a Command", "MEessgae");
-                                    count++;
+                                    MessageBox.Show(Currentline.ToString() + "line "+ lines[count] + " not a Command", "MEessgae");
                                     break;
 
                             }
                             display.Refresh();
+                            myPen.Dispose();
+                            count++;
                         }
-                        catch 
+                        catch
                         {
                             count = lines.Length + 1;
                         }
-                    } 
-                 
+                    }
+                }
             }
         }
 
-
-        private void Form1_Load(object sender, EventArgs e)
+        private void Rectangle(int width, int hight)
         {
-
-        }
-
-        private void command_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
+            using (var g = Graphics.FromImage(display.Image))
             {
-                Run.PerformClick();
-                e.SuppressKeyPress = true;
-                e.Handled = true;
+                ; // get a reference to Graphics object
+                Pen myPen = new Pen(Color.Pink, 2);
+                g.DrawRectangle(myPen, 0, 0, width, hight);
             }
         }
-        private void drawCircle(PaintEventArgs e)
+        private void Circle(int radius) 
         {
-            Graphics g = e.Graphics; // get a reference to Graphics object
-            Pen myPen = new Pen(Color.Pink, 2);
-
-
-            g.DrawEllipse(myPen, 50, 50, 200, 100);
-
+            using (var g = Graphics.FromImage(display.Image)) { 
+                          
+            g.DrawEllipse(pen, 50, 50, 2*radius, 2 * radius);
+            }
         }
 
 
-        private void Run_Paint(object sender, PaintEventArgs e)
+        private void f11(object sender, KeyPressEventArgs e)
         {
-
+            if (e.Equals(Keys.Enter) )
+            {   // need to chenck if run is used in bottom input if not do thing.
+                //move switch in to own methord called checker.
+                // need to remove run button and need change event.
+                Run.PerformClick();
+            }
         }
 
-        private void display_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void display_Click(object sender, EventArgs e)
+        private void textBox1_Enter(object sender, EventArgs e)
         {
 
         }
     }
+    
 }
