27db37a97182f277a7f7dd1f6910c95a594f4acd
Cryerjw01 <56078733+Cryerjw01@users.noreply.github.com>
2019-11-01 11:16:50 +0000

    TEST DIDNT WORK WHEN PULLED FROM GITHUB SO REMADE THEM

---
+++ GPLATEST/GPLA_TESTS.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using GPLA;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Forms;
+
+namespace GPLATEST
+{
+    [TestClass]
+    public class GPLA_TESTS
+    {
+        [TestClass]
+        public class GPLATests
+        {
+
+            [TestMethod]
+            public void _draw_shapes_with_variables()
+            {
+                ArrayList Currentline = new ArrayList();
+                String[] lines = { "Circle 100", "Square 100", "Rectangle 10 20", "Triangle 40 50 60" };
+                string[] line;
+                int i = 0;
+                while (lines.Length > i)
+                {
+                    line = lines[i].Split(' ');
+                    Currentline.Add(line);
+                    i++;
+                }
+                Form1 test = new Form1();
+                test.Check(Currentline, lines);
+
+            }
+            [TestMethod]
+            public void draw_shapes_no_variables()
+            {
+                ArrayList Currentline = new ArrayList();
+                String[] lines = { "Circle", "Square", "Rectangle", "Triangle" };
+                string[] line;
+                int i = 0;
+                while (lines.Length > i)
+                {
+                    line = lines[i].Split(' ');
+                    Currentline.Add(line);
+                    i++;
+                }
+                Form1 test = new Form1();
+                test.Check(Currentline, lines);
+
+
+            }
+            /*
+            [TestMethod]
+           public void runs_commandline_on_enter()
+          {
+               object s = null;
+               Keys k = new Keys();
+             
+
+                Form1 test = new Form1();
+
+                 test.Commandline_KeyDown(s, SendKeys.Send("{ENTER}") );// cant simulate keypress.
+
+            }
+            */
+        }
+    }
+}
---
+++ GPLATEST/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GPLATEST")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GPLATEST")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("35604032-df2e-46b1-b3d3-3fe90da2b4f6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
