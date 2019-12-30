d463cd92702834e90d08bc704c0319ca045966c2
Cryerjw01 <56078733+Cryerjw01@users.noreply.github.com>
2019-12-30 15:24:05 +0000

    user can declear vars and loop started

--- GPLA/Form1.Designer.cs
+++ GPLA/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.display = new System.Windows.Forms.PictureBox();
             this.ControlePanel = new System.Windows.Forms.RichTextBox();
             this.commandline = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.display)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,7 +40,7 @@
             // 
             this.display.Location = new System.Drawing.Point(322, 12);
             this.display.Name = "display";
-            this.display.Size = new System.Drawing.Size(466, 426);
+            this.display.Size = new System.Drawing.Size(479, 470);
             this.display.TabIndex = 6;
             this.display.TabStop = false;
             // 
@@ -58,11 +60,33 @@
             this.commandline.TabIndex = 8;
             this.commandline.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Commandline_KeyDown);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(225, 440);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(144, 439);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(813, 494);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.commandline);
             this.Controls.Add(this.ControlePanel);
             this.Controls.Add(this.display);
@@ -78,6 +102,8 @@
         private System.Windows.Forms.PictureBox display;
         private System.Windows.Forms.RichTextBox ControlePanel;
         private System.Windows.Forms.TextBox commandline;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- GPLA/Form1.cs
+++ GPLA/Form1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -16,14 +17,17 @@ using System.Windows.Forms;
 
 namespace GPLA
 {
-    
+
     public partial class Form1 : Form
     {
-        
-        private readonly Pen pen = new Pen(Color.Cyan, 2);
-        private Brush brush = new SolidBrush(Color.Black);
-        private int x=0, y=0;
-        
+
+       
+       
+        private int x = 0, y = 0;
+        private static int varCount = 0;
+        private  string[] vars = new string[50];
+        private  string[] varsParams = new string[50];
+
 
         public Form1()
         {
@@ -33,14 +37,17 @@ namespace GPLA
 
         }
 
-        private void Check( ArrayList Currentline, String[]lines)
+        private void Check( ArrayList Currentline)
         {
+
             using (var g = Graphics.FromImage(display.Image))
             {
                 int count = 0;
                 while (lines.Length >= count)
                 {
-                    
+                    Pen pen = new Pen(Color.Cyan, 2);
+                    Brush brush = new SolidBrush(Color.Black);
+
                     try
                     {
                         string[] element = (String[])Currentline[count];
@@ -164,8 +171,67 @@ namespace GPLA
                                 int.TryParse(element[1], out y);
 
                                 break;
-                            default:
-                                MessageBox.Show(element[0] + " not a Command", "MEessgae");
+
+                            case "var":
+                                VarCheck(element[1],element[2]);
+                                break;
+                            case "loop":
+                                int loopmax=0;
+                                int next = count;
+                                int.TryParse(element[1],out loopmax);
+
+                                next++;
+                                int k=0;
+                                ArrayList loopedcommands= new ArrayList();
+
+                                if (element[next] == "end")
+                                {
+                                    lopper(loopedcommands);
+                                }
+                                else
+                                {
+                                    loopedcommands[k] = Currentline[next];
+                                }
+                                
+
+                                
+                                break;
+                            default: 
+                                if (element[0].Trim() == null)
+                                {
+
+                                }
+                                else 
+                                {
+                                    int i = 0;
+                                    bool varthere = true;
+                                    if (vars[0] != null)
+                                    {
+                                        while (49 >= i)
+                                        {
+                                            if (vars[i] == element[0])
+                                            {
+                                                MessageBox.Show(element[0] + " called", "whoop");
+                                                i = 50;
+                                            }
+                                            else if (i != vars.Length)
+                                            {
+                                                i++;
+                                            }
+                                            else
+                                            {
+                                                varthere = false;
+                                            }
+                                        }
+                                    }
+                                    else
+                                    {
+                                        varthere = false;
+                                    }
+                                    if (varthere == false) {
+                                        MessageBox.Show(element[0] + " not a Command", "MEessgae");
+                                    }
+                                }
                                 break;
 
                         }
@@ -173,8 +239,10 @@ namespace GPLA
                     }
                     catch
                     {
-
+                  
                     }
+                    pen.Dispose();
+                    brush.Dispose();
                     count++;
 
                 }
@@ -183,6 +251,57 @@ namespace GPLA
                 
         }
 
+        private void lopper(ArrayList loopedcommands)
+        {
+            Check(loopedcommands);
+        }
+
+        private void VarCheck(string element1, string element2)
+        {
+            int i = 0;
+            try
+            {
+                if (vars[0] == null && varsParams[0] == null)  // arrays max is [49];
+                {
+                    vars[0] = element1;//name 
+                    varsParams[0] = element2;//value
+                }
+                else
+                {
+                    while (49 >= i)
+                    {
+                        if (vars[i].Equals(element1))//if names are the same
+                        {
+                            if (varsParams[i].Equals(element2))
+                            {
+                                MessageBox.Show("Variable already decleard", "Error");
+
+                                i = vars.Length;
+                            }
+                            else
+                            {
+                                
+                            }
+                        }
+                        else if (i >= vars.Length)
+                        {
+                            i++;
+                        }
+                        else
+                        {
+                            vars[i++] = element1;
+                            varsParams[i] = element2;
+                        }
+                    }
+                }
+              //  MessageBox.Show("var created", "DING DING DING!!!");
+            }
+            catch
+            {
+             //   MessageBox.Show("DIDNT LIKE SOMETHING ", "ERROR!!");
+            }
+            
+        }
 
         private void Commandline_KeyDown(object sender, KeyEventArgs e)
            {
@@ -221,7 +340,7 @@ namespace GPLA
                         
 
                     }
-                    Check(Currentline, lines);
+                    Check(Currentline);
                     display.Refresh();
                    
                 }
@@ -229,7 +348,43 @@ namespace GPLA
           
 
         }
-        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile1 = new OpenFileDialog();
+            openFile1.DefaultExt = "*.txt";
+            openFile1.Filter = "TXT Files|*.txt";
+            if (openFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK && openFile1.FileName.Length > 0)
+            {
+             ControlePanel.LoadFile(openFile1.FileName, RichTextBoxStreamType.PlainText);
+            }
+            openFile1.Dispose();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MemoryStream userInput = new MemoryStream(Encoding.UTF8.GetBytes(ControlePanel.Text));
+           
+            SaveFileDialog save = new SaveFileDialog();
+            save.DefaultExt = "txt";
+            save.Filter ="Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            DialogResult result = save.ShowDialog();
+            Stream fileStream;
+           
+            if (result == DialogResult.OK)
+            {                
+                
+                fileStream = save.OpenFile();
+                userInput.Position = 0;
+                userInput.WriteTo(fileStream);
+                fileStream.Close();
+                userInput.Close();
+                
+            }
+            save.Dispose();
+        }
+
+    
     }
     
 }
